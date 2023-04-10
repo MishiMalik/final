6523b84e03545a64b7a1db1ab6aa1d6cfec89501
Mehwish <mehwishgul715@gmail.com>
2023-04-10 08:41:44 +0500

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,137 @@
+
+
+$(".close-pop").on("click",function(){
+$(".fixed-partiper").css("bottom","-700px");
+})
+$(document).ready(function() {
+    $(".fixed-partiper").delay(2000).fadeIn(500);
+});
+// ===================================carsd
+$('.owl-carousel-tent').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
+   
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:2,
+            nav:true,
+            loop:false
+        }
+    }
+});
+$('.owl-carousel-news').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        },
+        
+    }
+});
+$('.owl-carousel-trips').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        },
+        
+    }
+});
+$('.owl-carousel-bus-trip').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:false
+        },
+        
+    }
+});
+
+// ========================chart
+  
+google.charts.load('current', {'packages':['bar']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['', '', '', '','','','','', '','','','',''],
+    ['2014', 1000, 400, 200, 300, 500, 300, 200, 1000, 400, 200, 400, 200],
+  
+  ]);
+
+  var options = {
+    
+    chart: {
+    //   title: 'Oktoberfest Turnout Forecast',
+      subtitle: '',
+    },
+    vAxis: {
+        minValue: 0,
+        gridlines: {
+          color:'transparent',
+          title:'none'
+        },
+     } 
+  };
+
+  var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+  chart.draw(data, google.charts.Bar.convertOptions(options));
+}
